f29ae6e007eceb14f08cccd4deb4ceb72b266429
RamilaJaneAbobo <168076651+RamilaJaneAbobo@users.noreply.github.com>
2024-04-29 15:13:04 +0800

    Add files via upload

---
+++ grp5_Abobo_ex1.py
@@ -0,0 +1,24 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+#1.)
+citcs_string = "citcs"
+result_list = [i for i in citcs_string]
+print(result_list)
+
+#2.)
+result_list = [i for i in range(1, 11)]
+print(result_list)
+
+#3.)
+list1= [2,4,6,8,10]
+print(list1)
+
+#4.)
+list2=[1,3,5,7,9]
+print(list2)
